77c7db0f904e236c7ce1b2ab110369f85049c809
Dmitrii Sobin <neo7976@yandex.ru>
2022-06-06 15:50:25 +0300

    Последнее обновление перед загрузкой

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Person person = new Person("Petya", 15);
+        System.out.println(person);
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " возраст: " + age;
+    }
 }
